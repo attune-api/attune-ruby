5eb21a5f9cfcca69d38b1354fd6d75e0a2f7cb43
John Hawthorn <john@freerunningtechnologies.com>
2013-12-18 14:57:05 -0800

    spec for Attune.{configure,test_mode}

--- spec/attune_spec.rb
+++ spec/attune_spec.rb
@@ -9,4 +9,18 @@ describe Attune do
     specify { expect(subject.endpoint).to eq 'http://localhost/' }
     specify { expect(subject.disabled).to eq false }
   end
+  describe 'configure' do
+    it "yields with Attune::Default" do
+      Attune.configure do |c|
+        expect(c).to be Attune::Default
+      end
+    end
+  end
+  describe 'test_mode' do
+    it "sets correct test options" do
+      expect(Attune::Default).to receive(:exception_handler=).with(:mock)
+      expect(Attune::Default).to receive(:disabled=).with(true)
+      Attune.test_mode!
+    end
+  end
 end
