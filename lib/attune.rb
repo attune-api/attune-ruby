21d3c1f757ea6a808a54ea70ae03f56abcdee4b5
John Hawthorn <john@freerunningtechnologies.com>
2013-12-18 14:54:18 -0800

    Attune.configure and Attune.test_mode!

--- lib/attune.rb
+++ lib/attune.rb
@@ -9,4 +9,13 @@ module Attune
   def self.client
     Client.new
   end
+  def self.test_mode!
+    configure do |c|
+      c.exception_handler = :mock
+      c.disabled = true
+    end
+  end
+  def self.configure(&block)
+    Default.configure(&block)
+  end
 end
